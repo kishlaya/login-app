68d149b8659644f824f3d9e5b00f704936c54eae
Kishlaya Jaiswal <kishlaya.j@gmail.com>
2018-03-20 21:34:54 +0530

    Fetch mongodb server from config

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 var config = require('config');
-var port = config.get('server.port');
 
-mongoose.connect('mongodb://localhost/loginApp');
+mongoose.connect(config.get('mongodb'));
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection Error: '));
-db.once('open', function() {});
+db.once('open', function() {
+	console.log("MongoDB connection established");
+});
 
 app.use(session({
 	secret: 'work hard',
@@ -40,6 +41,7 @@ app.use(function (err, req, res, next) {
   res.render('error', {message: err.message});
 });
 
-app.listen(port, function() {
-	console.log("Listening on " + port);
+app.set('port', config.get('server.port'));
+app.listen(app.get('port'), function() {
+	console.log("Listening on " + app.get('port'));
 });
